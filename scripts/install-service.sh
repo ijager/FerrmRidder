8269848fda79521b0f48571558207119b58e0c86
Ferr'm Ridder <ridder@ferrm.nl>
2022-08-25 20:38:32 +0100

    Fix install script

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Copy systemd scripts to /etc/systemd/system/"
+echo "Copy systemd scripts to /lib/systemd/system/ridder.service"
 
-sudo cp $DIR/ridder.service /etc/systemd/system/ridder.service
+# because we need the script to run as root
+sudo cp $DIR/ridder.service /lib/systemd/system/ridder.service
+sudo systemctl daemon-reload
