aa6bccadfb225b0dbc9c7c0527f71004be9dd0a2
Ferr'm Ridder <ridder@ferrm.nl>
2022-09-01 19:41:37 +0100

    Add improvements to randomly chosing wavfiles
    Too random is not good, so we'll keep a list of the last N choices.

--- src/audio.py
+++ src/audio.py
@@ -1,6 +1,7 @@
 import pygame
 import glob
 import random
+from collections import deque
 
 class RidderAudio:
 
@@ -20,19 +21,41 @@ class RidderAudio:
     self.startup = glob.glob(audiodir + '/startup/*.wav')
     print('found startupfiles:', self.startup)
 
+    N = (len(self.intermezzo) + len(self.dankjewel)) // 2
+
+    self.recently_played = deque(maxlen=N//2)
     self.sound = None
 
   def play_random_startup(self):
     f = random.choice(self.startup)
     return self.play(f)
 
-  def play_random_dankjewel(self):
-    f = random.choice(self.dankjewel)
+  def play_filtered_random(self, options):
+    """
+    Pick a random file from the list,
+    but make sure it is not in the list of recently played.
+    Because we don't really want true randomness..
+    """
+    tries = 10
+    f = None
+    while not f:
+      f = random.choice(options)
+      if f in self.recently_played:
+        tries -= 1
+        if tries == 0:
+          # too many tries, just use this pick then
+          break
+        # set f to None to try again
+        f = None
+
+    self.recently_played.append(f)
     return self.play(f)
 
+  def play_random_dankjewel(self):
+    return self.play_filtered_random(self.dankjewel)
+
   def play_random_intermezzo(self):
-    f = random.choice(self.intermezzo)
-    return self.play(f)
+    return self.play_filtered_random(self.intermezzo)
 
   def play_fallback(self):
     f = self.fallback[0]
@@ -54,4 +77,5 @@ class RidderAudio:
       self.sound.play()
       return (f, self.sound.get_length())
     except:
+      print("Error playing ", f)
       return self.play_fallback()
